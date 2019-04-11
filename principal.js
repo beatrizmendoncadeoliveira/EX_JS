0c3c58fa655eb014febd320f4399908bebe552ab
beatrizmendoncadeoliveira <47859767+beatrizmendoncadeoliveira@users.noreply.github.com>
2019-04-11 09:15:13 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,6 @@
+var a=document.querySelector("#titulo");
+a.textContent="Cacildis que bibliotequis maneris";
+a.style.color="red";
+
+var b=document.querySelector("header");
+b.style.background="grey";
